3b8ae232ebcf5e27e9f81fbaf3c0fafbc84d7af1
Chris <vanwieme@msu.edu>
2010-06-23 16:48:28 -0700

    remove unordered_list_dl filter. add recursive includes filter to conf details page.

--- secinv/apps/machines/templatetags/machine_extras.py
+++ secinv/apps/machines/templatetags/machine_extras.py
@@ -291,115 +291,18 @@ register.filter('diff_html', engine(utils.diff_html))
 register.filter('diff_table', engine(utils.diff_table))
 
 
-@register.filter
-def unordered_list_dl(value, autoescape=None):
-    """
-    Recursively takes a self-nested list and returns an HTML unordered list --
-    WITHOUT opening and closing <ul> tags.
-
-    The list is assumed to be in the proper format. For example, if ``var``
-    contains: ``['States', ['Kansas', ['Lawrence', 'Topeka'], 'Illinois']]``,
-    then ``{{ var|unordered_list_dl }}`` would return::
-
-        <li>States
-        <ul>
-                <li>Kansas
-                <ul>
-                        <li>Lawrence</li>
-                        <li>Topeka</li>
-                </ul>
-                </li>
-                <li>Illinois</li>
-        </ul>
-        </li>
-    """
-    if autoescape:
-        from django.utils.html import conditional_escape
-        escaper = conditional_escape
-    else:
-        escaper = lambda x: x
-    def convert_old_style_list(list_):
-        """
-        Converts old style lists to the new easier to understand format.
-
-        The old list format looked like:
-            ['Item 1', [['Item 1.1', []], ['Item 1.2', []]]
-
-        And it is converted to:
-            ['Item 1', ['Item 1.1', 'Item 1.2]]
-        """
-        if not isinstance(list_, (tuple, list)) or len(list_) != 2:
-            return list_, False
-        first_item, second_item = list_
-        if second_item == []:
-            return [first_item], True
-        old_style_list = True
-        new_second_item = []
-        for sublist in second_item:
-            item, old_style_list = convert_old_style_list(sublist)
-            if not old_style_list:
-                break
-            new_second_item.extend(item)
-        if old_style_list:
-            second_item = new_second_item
-        return [first_item, second_item], old_style_list
-
-    def _helper(list_, tabs=1):
-        indent = u'    ' * tabs
-        output = []
-
-        list_length = len(list_)
-        i = 0
-        while i < list_length:
-            title = list_[i]
-            sublist = ''
-            sublist_item = None
-            if isinstance(title, (list, tuple)):
-                sublist_item = title
-                title = ''
-            elif i < list_length - 1:
-                next_item = list_[i+1]
-                if next_item and isinstance(next_item, (list, tuple)):
-                    # The next item is a sub-list.
-                    sublist_item = next_item
-                    # We've processed the next item now too.
-                    i += 1
-            if sublist_item:
-                sublist = _helper(sublist_item, tabs+1)
-                sublist = '\n%s<ul>\n%s\n%s</ul>\n%s' % (indent, sublist,
-                                                         indent, indent)
-            output.append('%s<li>%s%s</li>' % (indent,
-                    escaper(force_unicode(title)), sublist))
-#            output.append('%s<li>%s%s</li>' % (indent,
-#                    title[0], sublist))
-            i += 1
-        return '\n'.join(output)
-    value, converted = convert_old_style_list(value)
-
-    return mark_safe(_helper(value))
-
-unordered_list_dl.is_safe = True
-unordered_list_dl.needs_autoescape = True
-
-
-
-
-
 
 class GetNestedItemsNode(template.Node):
-    """
-    TODO: Merge tag with 'unordered_list_dl'.
-    """
     def __init__(self, nodelist, field_name, machine_id):
         self.nodelist = nodelist
         self.field_name = field_name
         self.machine_id = machine_id
 
     def render(self, context):
-        # Remove quotation marks from string value.
         field_name = self.field_name
         machine_id = self.machine_id
 
+        # Remove quotation marks from string values.
         if field_name[0] in ('"', "'") and field_name[0] == field_name[-1]:
             field_name = field_name[1:-1]
         else:
@@ -448,13 +351,6 @@ class GetNestedItemsNode(template.Node):
                 pass
 
             output += '%s\n' % line
-
-
-
-        #matches = re.findall(r'<span class="item">([^<]+)</span>', output)
-        #for m in matches:
-        #    print m
-
 
         return '%s' % output
 
