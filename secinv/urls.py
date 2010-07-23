4b518715b48ccc347d49d80f0661908af78fa75f
Chris <vanwieme@msu.edu>
2010-07-23 11:43:54 -0700

    add url pattern for statically serving media

--- secinv/urls.py
+++ secinv/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
@@ -13,8 +14,13 @@ urlpatterns = patterns('',
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-
-    # TODO: temporary redirect
-    (r'^', lambda request: HttpResponsePermanentRedirect('/secinv/machines/')),
 )
+
+if settings.DEBUG:
+    # Remove leading and trailing slashes so the regex matches.
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
 
