5e3816278527f92febd7a13c8d3dafe855f06671
Chris <vanwieme@msu.edu>
2010-08-03 10:30:52 -0700

    add site index

--- secinv/urls.py
+++ secinv/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
 from django.http import HttpResponsePermanentRedirect
@@ -7,6 +7,7 @@ from django.http import HttpResponsePermanentRedirect
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'secinv.apps.machines.views.index', name='index'),
     (r'^machines/', include('secinv.apps.machines.urls')),
     (r'^webapps/', include('secinv.apps.webapps.urls')),
     (r'^accounts/', include('secinv.apps.accounts.urls')),
