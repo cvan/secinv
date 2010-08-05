6ce5ed76d6e01516cf9956b5bad79fad399e39a6
Chris <vanwieme@msu.edu>
2010-08-05 15:49:25 -0700

    remove perm redirect include

--- secinv/urls.py
+++ secinv/urls.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
-from django.http import HttpResponsePermanentRedirect
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
