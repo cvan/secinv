0a70cc436e3d83acd1543ace130dd5b8c76b68c0
Chris <vanwieme@msu.edu>
2010-08-05 13:20:42 -0700

    remove secinv prefix

--- secinv/apps/webapps/urls.py
+++ secinv/apps/webapps/urls.py
@@ -4,9 +4,11 @@ from . import views
 application_id = r'(?P<application_id>[-\w]+)'
 assessment_id = r'(?P<assessment_id>[-\w]+)'
 
-urlpatterns = patterns('secinv.apps.webapps.views',
+urlpatterns = patterns('apps.webapps.views',
     url(r'^$', 'index', name='webapps-index'),
-    url(r'^applications/%s/$' % application_id, 'application', name='application'),
-    url(r'^applications/%s/assessments/%s/$' % (application_id, assessment_id), 'assessment', name='assessment'),
+    url(r'^applications/%s/$' % application_id, 'application',
+        name='application'),
+    url(r'^applications/%s/assessments/%s/$' % (application_id, assessment_id),
+        'assessment', name='assessment'),
 )
 
